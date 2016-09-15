b2d545efd9b20ac6740c141628818dc3d719b501
Markus Wolf <markus@wolf.place>
2016-09-15 22:43:58 +0200

    license

--- bin/motion.pl
+++ bin/motion.pl
@@ -1,5 +1,22 @@
 #!/usr/bin/perl
 
+# Copyright (C) 2016 Markus Wolf <roomybox@wolf.place>
+#
+# This file is part of roomyBox.
+#
+# roomyBox is free software: you can redistribute it and/or  modify
+# it under the terms of the GNU Affero General Public License, version 3,
+# as published by the Free Software Foundation.
+#
+# roomyBox is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with roomyBox.  If not, see <http://www.gnu.org/licenses/>.
+
+
 use strict;
 use warnings;
 use Device::BCM2835;
--- lib/Voice.pm
+++ lib/Voice.pm
@@ -1,3 +1,20 @@
+# Copyright (C) 2016 Markus Wolf <roomybox@wolf.place>
+#
+# This file is part of roomyBox.
+#
+# roomyBox is free software: you can redistribute it and/or  modify
+# it under the terms of the GNU Affero General Public License, version 3,
+# as published by the Free Software Foundation.
+#
+# roomyBox is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with roomyBox.  If not, see <http://www.gnu.org/licenses/>.
+
+
 package Voice;
 
 use Moose;
